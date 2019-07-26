8907086dbf1920fc6c6f547a2ac9f2829868e6f1
Divyanathan <asdivyanathan@gmail.com>
2019-07-27 03:03:05 +0530

    Done with Builder pattern

---
+++ src/creational/pattern/builder/pattern/Bank.java
@@ -0,0 +1,221 @@
+package creational.pattern.builder.pattern;
+
+
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Builder pattern is used to reduce the complexity of object creation.Just think that you class with value
+ * and you you are using constructor to initialize the value. in this case we have to very conscious while creating on object
+ * we have to give the argument in same order. here there is chance that we can make mistake.
+ * for example: instead of sending first name we could send last name
+ *
+ * And just think that we out of 10 value we need only 3 values required but still we have to send other values as null.
+ * So here in Builder pattern we can create on object with required field that is one of the biggest advantage
+ *
+ * In Builder pattern we have to follow certain rules
+ * 1)Create static inner class Ends with builder
+ * 2)Outer class constructor should be private
+ * 3)Create build method in inner class where we can create object of outer class
+ */
+class Bank {
+
+    private int accountNumber;
+    private String accountName;
+    private String bankName;
+    private String ifscCode;
+    private String branch;
+
+    private Bank(){}
+    private Bank(BankBuilder pBankBuilder) {
+        accountNumber   = pBankBuilder.accountNumber;
+        accountName     = pBankBuilder.accountName;
+        bankName        = pBankBuilder.bankName;
+        ifscCode        = pBankBuilder.ifscCode;
+        branch          = pBankBuilder.branch;
+    }
+
+    /**
+     * This method is used to display the Bank details
+     */
+    public void show(){
+        if (accountName!=null)
+            System.out.println("Account name "+accountName);
+        if (bankName!=null)
+            System.out.println("Bank name "+bankName);
+        if (ifscCode!=null)
+            System.out.println("IFSC CODE "+ifscCode);
+        if (branch!=null)
+            System.out.println("Branch "+branch);
+
+        List<Integer> l = new List<Integer>(){
+
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return false;
+            }
+
+            @Override
+            public Iterator<Integer> iterator() {
+                return null;
+            }
+
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @Override
+            public <T> T[] toArray(T[] a) {
+                return null;
+            }
+
+            @Override
+            public boolean add(Integer integer) {
+                return false;
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(Collection<? extends Integer> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(int index, Collection<? extends Integer> c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public Integer get(int index) {
+                return null;
+            }
+
+            @Override
+            public Integer set(int index, Integer element) {
+                return null;
+            }
+
+            @Override
+            public void add(int index, Integer element) {
+
+            }
+
+            @Override
+            public Integer remove(int index) {
+                return null;
+            }
+
+            @Override
+            public int indexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public int lastIndexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public ListIterator<Integer> listIterator() {
+                return null;
+            }
+
+            @Override
+            public ListIterator<Integer> listIterator(int index) {
+                return null;
+            }
+
+            @Override
+            public List<Integer> subList(int fromIndex, int toIndex) {
+                return null;
+            }
+        };
+    }
+
+    public static class BankBuilder{
+        private int accountNumber;
+        private String accountName;
+        private String bankName;
+        private String ifscCode;
+        private String branch;
+
+        public BankBuilder(int accountNumber) {
+            this.accountNumber = accountNumber;
+        }
+
+        public BankBuilder setAccountName(String pAccountName){
+            accountName = pAccountName;
+            return this;
+        }
+        public BankBuilder setBank(String pSetBank){
+            bankName = pSetBank;
+            return this;
+        }
+        public BankBuilder setIfscCode(String pSetIfscCode){
+            ifscCode = pSetIfscCode;
+            return this;
+        }
+        public BankBuilder setBranch(String pSetBranch){
+            branch = pSetBranch;
+            return this;
+        }
+        public Bank build(){
+            return new Bank(this);
+        }
+    }
+
+}
+
+class Demo {
+
+    public static void main(String[] args) {
+
+        Bank.BankBuilder bankBuilder = new Bank.BankBuilder(1912983);
+        bankBuilder.setAccountName("Divyanathan");
+        bankBuilder.setBank("ICICI");
+        bankBuilder.setBranch("Chennai");
+        bankBuilder.setIfscCode("ICC00034ND");
+
+        Bank lBank = bankBuilder.build();
+        lBank.show();
+    }
+}
